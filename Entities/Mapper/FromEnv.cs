33728b9afceccbdcd0705d7d2641f4b5139f1ab6
fuadmuhe12 <fuad.mohamed@aait.edu.et>
2024-07-11 17:44:24 +0300

    added mapper for ENV for EmailSettings

--- Entities/Mapper/FromEnv.cs
+++ Entities/Mapper/FromEnv.cs
@@ -2,11 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library_Management_System_BackEnd.Helper.Mail;
 
 namespace Library_Management_System_BackEnd.Entities.Mapper
 {
-    public class fromEnv
+    public static class FromEnv
     {
-        
+        public static EmailSettings MapToEmailSettings()
+        {
+            return new EmailSettings
+            {
+                Mail = Environment.GetEnvironmentVariable("SMTP_MAIL")!,
+                DisplayName = Environment.GetEnvironmentVariable("SMTP_DISPLAYNAME")!,
+                Password = Environment.GetEnvironmentVariable("SMTP_PASSWORD")!,
+                Host = Environment.GetEnvironmentVariable("SMTP_HOST")!,
+                Port = int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT")!)
+            };
+        }
     }
 }
